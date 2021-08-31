68aa24a67aab7a68add6d7453b14c719e2bbce63
ShakedYosef <85097819+ShakedYosef@users.noreply.github.com>
2021-08-31 08:50:02 +0300

    Update data.tf

--- modules/codebuild/data.tf
+++ modules/codebuild/data.tf
@@ -15,19 +15,6 @@ data "aws_iam_policy_document" "codebuild_assume_role_policy" {
 data "aws_iam_policy_document" "codebuild_role_policy" {
   statement {
     actions   = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:GetBucketVersioning",
-          "s3:PutObjectAcl",
-          "s3:PutObject"
-        ]
-    resources = [
-          "${data.aws_s3_bucket.codepipeline_bucket.arn}",
-          "${data.aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
-  }
-  statement {
-    actions   = [
             "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents",
